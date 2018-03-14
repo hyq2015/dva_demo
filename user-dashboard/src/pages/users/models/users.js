9cbdfe389aea8948602921b91735413b60794bbc
Ricky <505970021@qq.com>
2018-03-14 14:24:10 +0800

    用户编辑

--- user-dashboard/src/pages/users/models/users.js
+++ user-dashboard/src/pages/users/models/users.js
@@ -7,23 +7,14 @@ export default {
         total: null,
         page: null,
     },
-    /**
-     * 以 key/value 格式定义 reducer。用于处理同步操作，唯一可以修改 state 的地方。由 action 触发。
-     * */
     reducers: {
-        save(state, {payload: {data: list, total, page}}) {
-            console.log('返回请求结果3')
-            return {...state, list, total, page};
+        save(state, { payload: { data: list, total, page } }) {
+            return { ...state, list, total, page };
         },
     },
-    /**
-     * 以 key/value 格式定义 effect。用于处理异步操作和业务逻辑，不直接修改 state。
-     * 由 action 触发，可以触发 action，可以和服务器交互，可以获取全局 state 的数据等等。
-     * */
     effects: {
-        *fetch({payload: {page = 1}}, {call, put}) {
-            console.log('开始出发请求回调2')
-            const {data, headers} = yield call(usersService.fetch, {page});
+        *fetch({ payload: { page = 1 } }, { call, put }) {
+            const { data, headers } = yield call(usersService.fetch, { page });
             yield put({
                 type: 'save',
                 payload: {
@@ -33,17 +24,22 @@ export default {
                 },
             });
         },
+        *remove({ payload: id }, { call, put, select }) {
+            yield call(usersService.remove, id);
+            const page = yield select(state => state.users.page);
+            yield put({ type: 'fetch', payload: { page } });
+        },
+        *patch({ payload: { id, values } }, { call, put, select }) {
+            yield call(usersService.patch, id, values);
+            const page = yield select(state => state.users.page);
+            yield put({ type: 'fetch', payload: { page } });
+        },
     },
-    /**
-     * subscription 是订阅，用于订阅一个数据源，然后根据需要 dispatch 相应的 action。在 app.start() 时被执行，
-     * 数据源可以是当前的时间、服务器的 websocket 连接、keyboard 输入、geolocation 变化、history 路由变化等等
-     * */
     subscriptions: {
-        setup({dispatch, history}) {
-            return history.listen(({pathname, query}) => {
+        setup({ dispatch, history }) {
+            return history.listen(({ pathname, query }) => {
                 if (pathname === '/users') {
-                    console.log('开始出发请求回调1')
-                    dispatch({type: 'fetch', payload: query});
+                    dispatch({ type: 'fetch', payload: query });
                 }
             });
         },
--- user-dashboard/src/services/users.js
+++ user-dashboard/src/services/users.js
@@ -4,3 +4,14 @@ import request from '../utils/request';
 export function fetch({ page = 1 }) {
   return request(`/api/users?_page=${page}&_limit=${PAGE_SIZE}`);
 }
+export function remove(id) {
+    return request(`/api/users/${id}`, {
+        method: 'DELETE',
+    });
+}
+export function patch(id, values) {
+    return request(`/api/users/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(values),
+    });
+}
